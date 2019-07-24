ecb244ed1b52ea92540fcaf8fbf1fbd967e88bfa
Ali Masood <ahmad@zenex.co>
2019-07-25 01:37:41 +0900

    Unit test running

--- src/auto-grow.ts
+++ src/auto-grow.ts
@@ -1,6 +1,26 @@
 class AutoGrow extends HTMLElement {
+    public shadowRoot: any;
+    private root: any;
+
     constructor() {
         super();
+        const template = document.createElement('template');
+        template.innerHTML = this.template;
+        this.root = this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
+
+    connectedCallback() {
+
+    }
+
+    get template() {
+        return `
+     
+    <div>This is a div</div>
+
+    `;
+    }
+
 }
 window.customElements.define('auto-grow', AutoGrow);
